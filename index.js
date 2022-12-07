10f450263bd34d635667a451e6e1e70d8c0f3bb6
Your Name Main <you@example.com>
2022-12-07 00:08:14 -0800

    reducer added

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // defining const
 const INCREMENT = "INCREMENT"
 const DECREMENT ="DECREMENT"
-const ADD_USER ="ADD_USER"
 
 // state
 const initialCounterState ={
     count : 0,
     
 }
-const initialUsersState ={
-     name: [{name: "Bappy Sheikh"}]
-    
-}
+
 
 // action - object - type, payload
 
@@ -27,9 +23,23 @@ const DecrementCounter =()=>{
     }
 }
 
-const addUser =()=>{
- return {
-    type: ADD_USER,
-    payload: {name : 'sheikh'}
- }
+const counterReducer = (state=initialCounterState,action)=>{
+    switch (action.type) {
+        case INCREMENT:
+             return {
+                ...state ,
+                count: state.count + 1
+             }
+            // 
+        case DECREMENT:
+              return {
+                ...state ,
+                count: state.count - 1
+              }
+            // 
+            
+    
+        default:
+            state;
+    }
 }
